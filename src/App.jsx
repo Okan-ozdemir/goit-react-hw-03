5e02489058b0be9af8c97927187ef9d40b1c4ee1
Okan Özdemir <ookanozdemir@gmail.com>
2025-05-18 21:53:08 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import ContactForm from "./components/ContactForm/ContactForm";
+import ContactForm from "./components/ContactForm/ContactForm.jsx";
 import ContactList from "./components/ContactList/ContactList";
 import SearchBox from "./components/SearchBox/SearchBox";
 import "./App.css";
